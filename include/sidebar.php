2148cfeacac61c1383d5a5dec1cfdf9d0f86a02c
jialePhan <87068109+jialePhan@users.noreply.github.com>
2021-07-09 14:52:08 +0800

    Add files via upload

---
+++ include/sidebar.php
@@ -0,0 +1,20 @@
+<div style="margin-bottom: 30px;">
+<nav class="navbar navbar-default" >
+  <div class="container" >
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" >
+      <ul class="nav navbar-nav">
+	  <li ><a href="insert-product.php"><i class=""></i>Insert Product </a></li>
+	  <li><a href="manage-products.php"><i class=""></i>Manage Products </a></li>    	  
+	  <li><a href="change-password.php">Change Password</a></li>
+	  <li><a href="logout.php"><i class=""></i>Logout</a></li>  
+      </ul>
+      
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
+
+
+					
